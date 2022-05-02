16bef0463ba33df2ef72e3c914464f1981a36678
Weikun Wu <weikunw@hotmail.com>
2022-05-01 22:24:51 -0700

    solved problems

---
+++ 199/solution.java
@@ -0,0 +1,66 @@
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+// Example 2:
+
+// Input: root = [1,null,3]
+// Output: [1,3]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<TreeNode> cur = new ArrayList<>();
+        ArrayList<TreeNode> next = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        next.add(root);
+        while (!next.isEmpty()) {
+            cur = next;
+            next = new ArrayList<>();
+            TreeNode n = cur.get(0);
+            while (!cur.isEmpty()) {
+                n = cur.remove(0);
+                if (n.left != null) {
+                    next.add(n.left);
+                }
+                if (n.right != null) {
+                    next.add(n.right);
+                }
+            }
+            res.add(n.val);
+        }
+        return res;
+    }
+}
